99b55be28f9fbc2ff07937099b3837af30f03b21
James Cuzella <james.cuzella@lyraphase.com>
2017-12-18 18:15:34 -0700

    Improve mp4 conversion script: support command line flags & positional input/output options at bare minimum

--- phototimer-images-to-mp4.sh
+++ phototimer-images-to-mp4.sh
@@ -1,35 +1,139 @@
 #!/bin/bash
+###
+### phototimer-images-to-mp4.sh - Convert directory of time-lapse phototimer .jpg files into frame-timestamped .mp4
+###
+### Author: James Cuzella [@trinitronx](https://github.com/trinitronx/)
+### Copyright (C) 2016  James Cuzella
+### License: see the LICENSE file
+###
+SCRIPT="$0"
 
-if [[ $# -eq 0 ]]; then
- echo "Usage: $0 ./path/to/phototimer  /path/to/output.mp4 [true]" >&2
+show_license_boilerplate() {
+  local author='James Cuzella'
+  echo "" >&2
+  cat <<EOF >&2
+${0}  Copyright (C) $(date +%Y)  ${author}
+This program comes with ABSOLUTELY NO WARRANTY; for details type '${0} -l'.
+This is free software, and you are welcome to redistribute it
+under certain conditions; See 'LICENSE' file for details.
+EOF
+  echo "" >&2
+}
+
+show_license() {
+ local path_to_license="$(dirname $(basename ${SCRIPT}))/LICENSE"
  echo "" >&2
- echo "\$1    path to directory containing phototimer images/*" >&2
+ cat $path_to_license >&2
  echo "" >&2
- echo "\$2    path to output mp4 file" >&2
- echo "       NOTE: This will overwrite the output file without asking!" >&2
- echo "" >&2
- echo "\$3    Pass 'true' to add time & date stamp to bottom left of all image frames before transcoding." >&2
- echo "       NOTE: This is an irreversible process and will overwrite the original images!" >&2
- echo "             It is recommended to run this on a copy of the originals" >&2
- echo "" >&2
- echo "The phototimer directory should have strftime pattern: " >&2
- echo "  'images/%YYYY/%M/%d/%H/%YYYY_%M_%d_%H_%r.jpg'" >&2
- echo "" >&2
- echo "Example: " >&2
- echo "  'images/2017/6/9/18/2017_6_9_18_1497034611561.jpg'" >&2
- echo "" >&2
- echo "Note that this script is very minimal and will NOT handle every ordering case correctly"
- echo "" >&2
- echo "It will also overwrite /path/to/output.mp4 without asking!" >&2
- exit 1
+}
+
+# Function to show help text
+show_help() {
+  show_license_boilerplate
+
+  cat << EOF >&2
+Usage: ${SCRIPT} [-h] [-l] [-t] (-i input_phototimer_dir | ./path/to/phototimer )  (-o /path/to/output.mp4 | /path/to/output.mp4 )
+
+Sort a path of timestamped phototimer images by frame timestamp, create a symlinked directory structure, 
+optionally add a timestamp overlay to each frame, and convert into a time-lapse .mp4 (x264) video.
+
+All arguments except input directory and output file path are optional and have defaults.
+
+
+    -h                              Help. Display this usage message and exit.
+
+    -l                              Show LICENSE file and exit.
+
+    -v                              Output verbose debug messages.
+
+    -n                              Dry run.
+                                    Echo all processing operations, but do not actually do anything.
+
+    -t                              Add timestamp image overlay to each frame based on image naming scheme.
+                                    The default configuration is to add time & date stamp to bottom left of 
+                                    all image frames before transcoding.
+                                    NOTE: This is an irreversible process and will edit & overwrite the original images!
+                                          It is recommended to run this on a copy of the originals
+                                    See Example below for timestamp file path naming scheme
+
+    -i /path/to/phototimer/images/  Input path to directory containing time-lapse phototimer 'images/*'
+
+    -o /path/to/output.mp4          Output filename to use for .mp4 time-lapse video
+                                    NOTE: This script will overwrite the output file without asking!
+
+
+    The phototimer directory should have strftime pattern: 
+
+        'images/%YYYY/%M/%d/%H/%YYYY_%M_%d_%H_%r.jpg'
+
+    Example:
+
+        'images/2017/6/9/18/2017_6_9_18_1497034611561.jpg'
+
+    Note that this script is very minimal and will NOT handle every ordering case correctly
+
+    It will also overwrite /path/to/output.mp4 without asking!
+
+EOF
+}
+
+
+# parse options (if any exist)
+while getopts "hlvnti:o:" opt; do
+    case "$opt" in
+        h) show_help
+           exit 1
+           ;;
+        l) show_license
+           exit 1
+           ;;
+        v) DEBUG='true'
+           # Output verbose debug messages
+           ;;
+        n) DRY_RUN='echo DRY RUN:'
+           # If variable set, it adds echo prefix to all processing commands
+           ;;
+        t) TIMESTAMP_FRAMES='true'
+           # If variable set, it adds echo prefix to all processing commands
+           ;;
+        i) INPUT_DIR=$OPTARG
+           ;;
+        o) OUTPUT_FILE=$OPTARG
+           ;;
+        *) show_help
+           exit 1
+           ;;
+    esac
+done
+# Now remove the parsed options
+shift $(( OPTIND - 1 ))
+
+# Leftover arguments are numbered positional parameters
+# If -i and -o flags are not passed, use first 2 positional parameters as input & output
+PHOTOTIMER_DIR="${INPUT_DIR:-${1}}"
+OUTPUT_MP4="${OUTPUT_FILE:-${2}}"
+
+# 
+# Error handling before we start...
+if [ -z "$PHOTOTIMER_DIR" -o ! -d "$PHOTOTIMER_DIR/images" ]; then
+    show_help
+    echo "ERROR: Input file path must be a directory containing phototimer 'images/*' dir!" >&2
+    exit 1
 fi
 
+if [ -z "$OUTPUT_MP4" ] || ! echo "$OUTPUT_MP4" | grep -Eq '.mp4$'; then
+    show_help
+    echo "ERROR: Output file $OUTPUT_MP4 must be an .mp4 file!" >&2
+    exit 1
+fi
 
-PHOTOTIMER_DIR=$1
-OUTPUT_MP4=$2
-TIMESTAMP_FRAMES=$3
-#DRY_RUN='echo DRY RUN:'
-#DEBUG='true'
+if ( ! which ffmpeg 2>&1 >/dev/null ); then
+  show_help
+  echo "ERROR: Could not find ffmpeg command line utility installed on this system!" >&2
+  echo "ERROR: ffmpeg is required to be in your \$PATH when using this script!" >&2
+  echo "" >&2
+  exit 1
+fi
 
 num_files=$(find ${PHOTOTIMER_DIR}/images/ -type f -iname '*.jpg' | wc -l | awk '{ print $1 }')
 num_digits=$(echo -ne $num_files | wc -m | awk '{ print $1 }')
@@ -104,6 +208,7 @@ for x in $(find ${PHOTOTIMER_DIR}/images/ -type f -iname '*.jpg' | sed s#//*#/#g
   (( i++ ));
 done
 
+## TODO: Make these command line options with defaults?
 framerate=25
 size=2048x1536
 num_cpu=$(sysctl -n hw.ncpu)
@@ -133,7 +238,7 @@ if [[ "$TIMESTAMP_FRAMES" == 'true' ]]; then
   echo "INFO: Begin rendering date + timestamp to frames" >&2
   echo "WARN: This is an irreversible process and will overwrite the original images!" >&2
   echo "" >&2
-  python ~/bin/phototimer-images-timestamp-overlay-multithreaded.py
+  $DRY_RUN python ~/bin/phototimer-images-timestamp-overlay-multithreaded.py
 fi
 
 echo "INFO: Begin Encoding frames to x264 video" >&2
